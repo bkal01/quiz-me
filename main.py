97635ce48638ac41a06a18c3c6531d2777cbce20
Bhavesh Kalisetti <bhaveshkalisetti@gmail.com>
2024-10-04 15:23:28 -0700

    main: add argparse logic
    adds logic to have two command line args: gen and eval. creates helper fn to perform generation work

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import os
 
+from argparse import ArgumentParser, Namespace
 from datetime import date
 from dotenv import load_dotenv
+from tqdm import tqdm
 
 from db.connection import get_connection
 from notion.extractor import NotionExtractor
 from quiz.quiz_generator import QuizGenerator
 
-def main() -> None:
+def generate(args: Namespace):
     load_dotenv()
-    
     cnx = get_connection()
     
     extractor = NotionExtractor(
         cnx=cnx,
         sources=[os.environ["ML_PAPER_NOTES_DB_ID"]]
     )
-    
+    print("Fetching any new pages from Notion to add for review...")
     extractor.pull_new_pages_from_source(extractor.sources[0])
     
     with open(os.environ["QUIZ_GENERATION_SYSTEM_PROMPT_PATH"]) as f:
@@ -28,6 +29,7 @@ def main() -> None:
         system_prompt=quiz_generator_system_prompt,
         user_prompt=quiz_generator_user_prompt,
     )
+    print("Getting review items due today...")
     due_review_items = generator.get_review_items_due_today()
     
     quiz_save_dir = f"generated_quizzes/{date.today().strftime('%m_%d_%Y')}"
@@ -35,27 +37,40 @@ def main() -> None:
         name=quiz_save_dir,
         exist_ok=True
     )
-    responses_save_dir = f"quiz_responses/{date.today().strftime('%m_%d_%Y')}"
-    os.makedirs(
-        name=responses_save_dir,
-        exist_ok=True
-    )
-    for item in due_review_items:
-        print(f"Processing {item.page_id}")
+    for item in tqdm(due_review_items):
+        print(f"Fetching content for {item.page_id}...")
         content = extractor.extract(
             block_id=item.page_id,
         )
         
+        print(f"Generating quiz for {item.page_id}")
         quiz = generator.generate_quiz(
             content=content,
         )
+        
+        print(f"Saving quiz for {item.page_id}...")
         with open(f"{quiz_save_dir}/{item.page_id}.txt", "w") as f:
-            f.write(quiz.title)
-            f.write("\n\n")
+            f.write(f"{quiz.title}\n\n")
             for question in quiz.questions:
-                f.write(question.question_text)
-                f.write("\n\n")
+                f.write(f"Q: {question.question_text}\n\n")
+                f.write("A: \n\n")
 
-        open(f"{responses_save_dir}/{item.page_id}.txt", "w")
+def evaluate(args: Namespace):
+    print("Not implemented yet.")
+
+def parse_arguments()-> Namespace:
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers()
+    quiz_generation_subparser = subparsers.add_parser("gen", help="generate your quizzes due today")
+    quiz_generation_subparser.set_defaults(func=generate)
+    quiz_evaluation_subparser = subparsers.add_parser("eval", help="grade your completed quizzes for today")
+    quiz_evaluation_subparser.set_defaults(func=evaluate)
+    
+    return parser.parse_args()
+
+def main(args: Namespace) -> None:
+    args.func(args)
+
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    main(args)
